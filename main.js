e87ec3f1d28fc95bc52880251feeaedc734bc5b5
ibrahimalihamim <mdibrahimalihamim@gmail.com>
2024-02-05 13:12:48 +0600

    Constructor function & how it works inside class

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ laptop.freeStorage(240,86);
 
 // ----------CLASS(Like a blue print of an object) & how it works-----
 
+/*----------
 class laptop{
   brand="Apple"
   ram=8
@@ -31,3 +32,19 @@ let LaptopObj = new laptop();
 LaptopObj.freeStorage(300,200);
 console.log(LaptopObj.brand);
 console.log(LaptopObj.ram);
+--------*/
+
+
+// -------Constructor function & how it works inside class
+
+class Laptop{
+  //This bonus function will execute automatically
+  constructor(a,b){
+    // console.log("I am constructor");
+    let c=a+b;
+    console.log(c);
+  }
+}
+
+let obj = new Laptop() // () is called constructor
+
